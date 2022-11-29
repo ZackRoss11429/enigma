0c6776bafb1617cec804b25d997f9e3124195eb1
ZackRoss11429 <115795499+ZackRoss11429@users.noreply.github.com>
2022-11-29 10:25:32 +0000

    Update Crytograph.py

--- Crytograph.py
+++ Crytograph.py
@@ -1,3 +1,17 @@
+# Plugboard that connects 10 pairs of letters together so when a letter is inputted, if it's one of the substituted pairs 
+# then it will be replaced as the other letter of its pair
+# 5 rotors that each have a substitution cipher (reordering of the alphabet). 3 can be chosen and placed in any order.
+# Each rotor have 26 letters to shift the inputted letter to depending on what the rotor index is set to
+# Ring settings which decides what order the substitution cipher starts in
+# Inputted letter is shifted first, substituted then placed into the next rotor.
+# When a letter has passed through the rotors, it will go through the reflector's substitution cipher then ran backwards through the rotors
+# Once that process is done, the first rotor will rotate by one. After it reaches the letter which allows the next rotor to move, the second
+# rotor rotates by one. Same process for the third rotor.
+# Afterwards, it will go through the plugboard once more before appearing as the final letter in the lamp board
+
+
+
+
 import numpy as np
 
 plugboard = [["a", "f"], ["b", "b"], ["c", "z"], ["d", "d"], ["e", "e"], ["f", "a"], ["g", "m"], ["h", "i"], ["i", "h"],
